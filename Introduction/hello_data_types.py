45f93d6a6fb0f0e429cd4005aef929d8b19df3d3
Juxhen Bica <juxhen.bica@gmail.com>
2020-07-10 12:08:46 +0100

    Lunch time updates

---
+++ Introduction/hello_data_types.py
@@ -0,0 +1,39 @@
+# print(14 % 3)
+# print(2 * 2)
+# print(12 / 3)
+# print(3 > 5)
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+
+# ------ NUMBERS ------
+# #int
+# a = 1
+# print(type(a))
+# #float
+# b = 2.5
+# print(type(b))
+# #complex
+# c = 3+2j
+# print(type(c))
+
+# ------ Strings ------
+
+# ------ Strings Methods Functions ------
+
+white_space = "      Here is a string with a bunch of white space    "
+print(len(white_space))
+#strip removes white space
+print(white_space.strip())
+print(white_space.lstrip())
+print(white_space.rstrip())
+
+print(white_space.count(" "))
+print(white_space.count("a"))
+print(white_space.count("bunch"))
+
+#------ Concatination --------
---
+++ Introduction/hello_variables.py
@@ -0,0 +1,56 @@
+# a = 5
+# b = 2
+# c = "Hello!"
+# d = 0.25
+# e = True
+# f = [1,2,3]
+# g = (1,2,3)
+# h = {1,2,3}
+#
+# print(a)
+# print(b)
+# print(c)
+#
+# # How to find the type of variables
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+# print(type(h))
+# CTRL + / To comment out a block of code
+
+# print("What is your name?")
+# name = input()
+# print(name)
+
+# Use input to collect name, age and DOB from user & display them
+#
+# print("What is your name?")
+# name = input()
+# nameType = type(name)
+# print("What is your age?")
+# age = input()
+# ageType = type(age)
+# print("What is your d.o.b, format DD/MM/YY")
+# dob = input()
+# dobType = type(dob)
+# print(name, age, dob)
+# print(dobType)
+#
+#-----------------------------------------------------------------------
+
+print("What is your name")
+name = input()
+print("What is your age")
+age = input()
+print("Siblings")
+siblings = input()
+print("Favourite decimal")
+decimal = input()
+print("Favourite animal")
+animal = input()
+
+print(f"Hi {name} you're {age} and you have {siblings} your fav dec is {decimal} and your fav animal is {animal}")
