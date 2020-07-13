34c04d3cee3eee09bacd9c38c690f23fa4800ee9
Juxhen Bica <juxhen.bica@gmail.com>
2020-07-13 22:25:55 +0100

    it works but not in a class

--- OOP/hangmanGame.py
+++ OOP/hangmanGame.py
@@ -6,18 +6,68 @@ import time as t
 secret_list = []
 user_guess_list = []
 display_list = []
-lives = 70
-
-def health(lives, damage_taken):
-    if lives - damage_taken < 0:
-        newHealth = 0
-        print("gg you're dead! - Try again")
-    else:
-        newHealth = lives - damage_taken
-        print(f"sorry you lost {damage_taken} HP you are at {newHealth} HP")
-    return newHealth
-
+lives = 100
 winning_word = hl.winning_word.get_word()
+
+print("\n")
+print("Welcome to Juxhen's Random Number Generator Hangman\n")
+
+
+def user():
+    name = input("Please enter your name\n").upper()
+    if name.isalpha():
+        print(f"Welcome {name} I wish you the best of luck lets start!")
+        return True
+    else:
+        print("Please enter letters only!")
+        return False
+
+
+name_check = user()
+while not name_check:
+    user()
+
+
+def levels():
+    level_choice = ""
+    flag = False
+    while not flag:
+        level_choice = input("Please pick a level, 'Easy', 'Medium', 'Hard'\n").upper()
+        if level_choice == "EASY":
+            print("Easy it is!")
+            return 100
+            flag = True
+        elif level_choice == "MEDIUM":
+            print("Medium it is!")
+            return 70
+            flag = True
+        elif level_choice == "HARD":
+            print("Hard it is!")
+            return 50
+            flag = True
+        else:
+            print("Please enter a valid difficulty")
+            flag = False
+
+level_check = levels()
+
+if level_check == 100:
+    lives = 100
+elif level_check == 70:
+    lives = 70
+elif level_check == 50:
+    lives = 50
+
+
+def health(lives_in_health, damage_taken):
+    if lives_in_health - damage_taken < 0:
+        new_health = 0
+        print(f"Congratulations, you're dead, the word was: {winning_word}")
+    else:
+        new_health = lives_in_health - damage_taken
+        print(f"sorry you lost {damage_taken} HP you are at {new_health} HP")
+    return new_health
+
 
 for letter in winning_word:
     secret_list.append(letter)
@@ -25,7 +75,8 @@ for letter in winning_word:
 
 for blanks in range(0, len(secret_list)):
     display_list.append("_")
-print(display_list)
+    display_list_str = " ".join(display_list)
+print(display_list_str)
 
 word_completed = False
 
@@ -35,8 +86,8 @@ while not word_completed and lives > 0:
         if user_guess in user_guess_list:
             print(f"sorry you have already guessed {user_guess} please pick another letter")
         elif user_guess not in secret_list:
-            healthlossrng = r.randint(5,10)
-            lives = health(lives, healthlossrng)
+            health_loss_rng = r.randint(1,10)
+            lives = health(lives, health_loss_rng)
         else:
             user_guess_list.append(user_guess)
             index_counter = 0
@@ -45,7 +96,10 @@ while not word_completed and lives > 0:
                     display_list[index_counter] = letter
                 index_counter += 1
             print(f"Horray you matched the letter {user_guess}")
-            print(f"{display_list}")
-
+            if "_" not in display_list:
+                print(f"Congratulations, the word was: {winning_word}")
+                word_completed = True
+            display_list_str = " ".join(display_list)
+            print(f"{display_list_str}")
     else:
         print(f"sorry {user_guess} is not a letter")
---
+++ OOP/hangman_game2.0.py
@@ -0,0 +1,4 @@
+import hangmanLogic as hl
+import random as r
+import time as t
+
