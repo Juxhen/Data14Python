55b038c843d8aa45443f8a340a61b68e0a892891
Juxhen Bica <juxhen.bica@gmail.com>
2020-07-18 16:25:35 +0100

    printing out the move and its effect

--- http_and_apis/using_requests_pokemon.py
+++ http_and_apis/using_requests_pokemon.py
@@ -13,24 +13,29 @@ for ability in url_for_abilities:
     result = ability['ability']
     list_of_abilities_urls.append(result['url'])
     list_of_abilities_names.append(result['name'])
-    print(f"The abilities are {result['url']}")
+    #print(f"The abilities are {result['url']}")
 
 for x in range(2):
     req_response_abilities = requests.get(list_of_abilities_urls[x])
     get_ability_in_abilities = req_response_abilities.json()
-    pprint(get_ability_in_abilities)
+    #pprint(get_ability_in_abilities)
 
-print("Pikachu has the following moves, which have the following effects")
+#print("Pikachu has the following moves, which have the following effects")
 
 list_of_effect = []
 for x in range(2):
     effect_changes = list_of_abilities_urls[x]
     req_response_effects = requests.get(effect_changes)
     url_for_effects = req_response_effects.json()['effect_entries']
+    for effects in url_for_effects:
+        #result = effects['effect']
+        result = effects['short_effect']
+        list_of_effect.append(result)
+        #pprint(list_of_effect)
 
-for effects in url_for_effects:
-    result = effects['effect']
-    list_of_effect.append(result)
-    pprint(list_of_effect)
-
-# effect_entries -> effect[1]
+print("\n")
+pprint(f"Pikachus first ability is: {list_of_abilities_names[0]},"
+        f"and the effect {list_of_effect[1]}")
+print("\n")
+pprint(f"Pikachus second ability is: {list_of_abilities_names[1]},"
+        f"and the effect {list_of_effect[-1]}")
