349165c6a56760ad213dd811e86ff74d38b0dfd2
Juxhen Bica <juxhen.bica@gmail.com>
2020-07-19 21:15:52 +0100

    finished, no tests had to do homework, will come back

--- pokemon+tests/pokemon.py
+++ pokemon+tests/pokemon.py
@@ -1,7 +1,5 @@
 from json import JSONDecodeError
 import requests
-import json
-from pprint import pprint
 
 
 class PokemonStuff:
@@ -31,6 +29,9 @@ class PokemonStuff:
         except JSONDecodeError:
             print("This pokemon doesn't exist")
 
+    # Gets the effect of each ability assigned to each pokemon where the language of that effect is in English,
+    # it appends to 2 lists, one with the ability and one with the corresponding abilities effect in English.
+    # For each row in the dictionary effect_entries search language and name where it's equal to 'en' - line 46
     def get_abilities_and_effects(self):
         for row in self.all_info['abilities']:
             self.list_of_abilities_names.append(row['ability']['name'])
@@ -43,22 +44,28 @@ class PokemonStuff:
                     short_effect = row['short_effect']
                     self.list_of_effect.append(short_effect)
 
+    # Gets the number of games the selected pokemon has appeared in, along with a list of all the versions names.
     def get_name_of_games_appeared_in(self):
         for row in self.all_info['game_indices']:
             self.list_of_game_indices.append(row['game_index'])
         for game_name in self.all_info['game_indices']:
             self.list_of_game_names.append(game_name['version']['name'])
 
+    # Gets the base stats of each pokemon and appends it to 1 list,
+    # and then checks each row for the name of that base stat which appends to a second list.
     def get_base_stats(self):
         for row in self.all_info['stats']:
             self.list_of_base_stats.append(row['base_stat'])
         for row2 in self.all_info['stats']:
             self.list_of_base_stats_names.append(row2['stat']['name'])
 
+    # Gets the name of the type of pokemon selected and appends to a list.
     def get_pokemon_type(self):
         for row in self.all_info['types']:
             self.list_of_pokemon_type.append(row['type']['name'])
 
+    # Opens a new txt file with the name of the pokemon entered and prints,
+    # a custom message for each pokemon's number of abilities ranging from 1 to 4.
     def appened_abilities_and_effects_to_txt_file(self):
         with open(f"pokemonapi_{self.pokemon_name}.txt", "a") as pokeapi:
             pokeapi.writelines("\n")
@@ -87,6 +94,7 @@ class PokemonStuff:
                 pokeapi.writelines(f"{self.pokemon_name.capitalize()}'s fourth ability is: {self.list_of_abilities_names[3]}, and the Effect: {self.list_of_effect[3]}")
                 pokeapi.writelines("\n")
 
+    # Appends to the text file created and adds the number of games and what games the pokemon has appeared in.
     def appened_name_of_games_appeared_in(self):
         with open(f"pokemonapi_{self.pokemon_name}.txt", "a") as pokeapi:
             pokeapi.writelines("\n")
@@ -94,6 +102,7 @@ class PokemonStuff:
             pokeapi.writelines(f"{self.pokemon_name.capitalize()} has appeared in {len(self.list_of_game_indices)} Pokemon games\n")
             pokeapi.writelines(f"The games appeared in are: {self.list_of_game_names}")
 
+    # Appends to the text file created and adds the pokemons base stats.
     def appened_base_stats(self):
         with open(f"pokemonapi_{self.pokemon_name}.txt", "a") as pokeapi:
             pokeapi.writelines("\n")
@@ -102,18 +111,11 @@ class PokemonStuff:
                 pokeapi.writelines("\n")
                 pokeapi.writelines(f"{self.list_of_base_stats_names [x]} : {self.list_of_base_stats [x]}")
 
+    # Creates the pokemon text file as it's the first method called in the __init__,
+    # it also displays the pokemon name and type.
     def appened_pokemon_type(self):
         with open(f"pokemonapi_{self.pokemon_name}.txt", "a") as pokeapi:
             pokeapi.writelines(f"------------------------------{self.pokemon_name.upper()}------------------------------")
             pokeapi.writelines("\n")
             pokeapi.writelines("\n")
             pokeapi.writelines(f"{self.pokemon_name.capitalize()} is an {self.list_of_pokemon_type} type Pokemon")
-
-
-# play = Pokemon()
-# play.get_abilities_and_effects()
-# play.appened_to_txt_file()
-# print("\n")
-# play.get_name_of_games_appeared_in()
-# print("\n")
-# play.get_base_stats()
